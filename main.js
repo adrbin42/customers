0258c8249c21a1d270f67eccfe501d1194a451fd
adrienne <adriennebing@gmail.com>
2017-06-14 16:25:17 -0400

    got all of user data in console

--- main.js
+++ main.js
@@ -6,5 +6,29 @@
   'use strict';
 
   // Your Code Goes Here
+  fetch("https://randomuser.me/api/?results=12").then(function(response){
+      response.json().then(function(data){
+      let customers = document.querySelector(".customers");
+      let retObj = data;
+      let objArr = retObj.results;
+      console.log(objArr);
+      for(let i = 0;i<objArr.length;i++){
+
+        console.log(objArr[i].picture.thumbnail);
+        console.log(objArr[i].name.title);
+        console.log(objArr[i].name.first);
+        console.log(objArr[i].name.last);
+        console.log(objArr[i].email);
+        console.log(objArr[i].location.street);
+        console.log(objArr[i].location.city);
+        console.log(objArr[i].location.state);
+        console.log(objArr[i].location.postcode);
+        console.log(objArr[i].phone);
+      };
+    });
+  });
 
 })();
+
+// picture, name, email,location,phone
+//Object {street: "3415 blumenstraße", city: "braunschweig", state: "bayern", postcode: 46363}
